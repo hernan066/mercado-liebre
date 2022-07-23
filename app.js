a3638b6c49607ab17c1957a0140b4e2807ba3f1b
hernan0066 <hernan0066@gmail.com>
2022-07-23 18:57:42 -0300

    Add env

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require("express");
 const path = require("path");
 const app = express();
@@ -12,4 +13,4 @@ app.get("/login", (req, res) => res.sendFile(path.join(__dirname, "views", "logi
 app.use(express.static("public"));
 
 //init server
-app.listen(port, () => console.log("Server running in http://localhost:" + port));
+app.listen(process.env.PORT || port , () => console.log("Server running"));
